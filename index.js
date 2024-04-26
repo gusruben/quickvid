ade3ffb01517ccae29f2588b62bc73b84a452dd0
Gus Ruben <gusruben@protonmail.com>
2024-04-26 17:43:36 -0400

    Update summary prompt

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ const openai = new OpenAI(); // api key comes from .env (using dotenv)
 const PORT = 3000;
 const HOSTNAME = "0.0.0.0";
 
+const SUMMARIZE_PROMPT =
+	"Please provide a detailed summary of the YouTube \
+video, explaining each step or key point in the process. The summary should \
+cover all major aspects discussed in the video, including any demonstrations, \
+examples, or techniques presented. Be sure to provide clear explanations for each \
+part of the process.";
+
 async function getTranscript(url) {
 	const captions = await YoutubeTranscript.fetchTranscript(url);
 	const transcript = captions.map(caption => caption.text).join(" ");
@@ -25,23 +32,24 @@ app.post("/summarize", async (req, res) => {
 		return;
 	}
 
+	console.log(`Recieved summary request for ${url}`)
+
 	const transcript = await getTranscript(url);
 
 	const completion = await openai.chat.completions.create({
 		messages: [
 			{
 				role: "system",
-				content:
-					"Summarize the following video transcript. Output only the summary:",
+				content: SUMMARIZE_PROMPT,
 			},
 			{ role: "user", content: transcript },
 		],
 		model: "gpt-3.5-turbo",
 	});
-	console.log(completion.choices)
 
-	res.send(completion.choices)
-	// res.send(completion.data.choices[0].text);
+	console.log(completion.choices)
+	const summary = completion.choices[0].message.content;
+	res.send(summary);
 });
 
 app.use(express.static("frontend"));
